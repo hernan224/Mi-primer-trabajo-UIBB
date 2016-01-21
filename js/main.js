aa544c4e644e22cc3c88402e796b491cd1258996
hernan224 <hernan224@gmail.com>
2016-01-21 20:42:21 -0300

    Inicio del proyecto Mi primer trabajo - UIBB
    Primeras pantallas del sitio Mi primer trabajo.
    Son las pantallas de las secciones que van a funcionar con DB.
    
    listado-backend.html -> Pantalla que meustra el listado de alumnos,
    visible para las escuelas responsables de la carga de datos
    
    listado-frontend.html-> Listado de alumnos visible para las empresas.
    
    carga-alumno.html-> formulario para la carga de los datos de los alumnos
    
    vista-alumno.html-> vista individual de los datos de cada alumno.
    
    TODO:
    -Falta definir headers y footers
    -Falta armar el formulario de login
    -Falta definir las secciones estáticas
    -Falta desarrollar los formularios de contacto

---
+++ js/main.js
@@ -0,0 +1,74 @@
+ $(document).ready()
+ {
+     //Inicializando tooltips
+     $('[data-toggle="tooltip"]').tooltip();
+
+
+     //Cabiar forma en que se muestra el listado de alumnos
+     var btnVista = $('.btn-cambiar-vista');
+     var contenedorLista = $('#contenedorLista');
+
+     btnVista.on('click', function (e) {
+         e.preventDefault();
+         var btnActual = $(this);
+         var btnDisabled = btnActual.siblings('button[disabled]');
+         contenedorLista.toggleClass('vista-listado');
+         btnActual.attr('disabled', true).addClass('activo');
+         btnDisabled.attr('disabled', false).removeClass('activo');
+     });
+
+    //Mostrar/Ocultar filtros
+     var contenedorPrincipal = $('#bodyFiltro');
+     var btnMostrarFiltros = $('#mostrarFiltrosBtn');
+     var ocultarFiltros = $('.cerrar-filtros');
+
+     btnMostrarFiltros.on('click', function (e) {
+         e.preventDefault();
+         contenedorPrincipal.addClass('mostrar-filtros');
+     });
+     ocultarFiltros.on('click', function (e) {
+         e.preventDefault();
+         contenedorPrincipal.removeClass('mostrar-filtros');
+     });
+
+     //Marcar item alumno
+     var checkAlumno = $('input.check-alumno');
+
+    checkAlumno.on('click', function(){
+        var este = $(this);
+        var itemAlumno = este.parents('li.item-alumno');
+
+        if(este.prop("checked")){
+            itemAlumno.addClass('item-marcado');
+        }else{
+            itemAlumno.removeClass('item-marcado');
+        }
+    });
+
+     //Mostrar label en carga de datos
+     var inputAlumno = $('.cargar-alumno input.form-control');
+
+     inputAlumno.on('change', function(){
+         var este = $(this);
+         var estePadre = este.parents('.form-group');
+
+         if(este.val() != '' && !estePadre.hasClass('cargado')){
+             estePadre.addClass('cargado');
+         }
+         else if(este.val() == '' && estePadre.hasClass('cargado'))
+         {
+             estePadre.removeClass('cargado');
+         }
+     });
+
+
+     //Lanzar modal para confirmar eliminación de alumno
+     var btnEliminar = $('.btn-eliminar');
+     var modalEliminar = $('#confirmarEliminar');
+
+     btnEliminar.on('click', function(e){
+         e.preventDefault();
+         modalEliminar.modal();
+     });
+
+ }
